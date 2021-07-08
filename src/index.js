ddfbab1a673f4fce2c241a046634cac2e7fcf8fc
testmkastep <84389834+testmkastep@users.noreply.github.com>
2021-07-08 19:42:45 +0300

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-
 exports.min = function min (array) {
-  return 0;
+    let result = checkArr(array)
+    if (result !== false)
+        return result
+    let min = array[0]
+    array.forEach((item) => {
+        if (item < min)
+            min = item
+    })
+
+    return min
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result = checkArr(array)
+    if (result !== false)
+        return result
+    let max = array[0]
+    array.forEach((item) => {
+        if (item > max)
+            max = item
+
+    })
+
+    return max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let result = checkArr(array)
+    if (result !== false)
+        return result
+    let n = array.length
+    let sum = 0;
+    array.forEach((item) => {
+        sum += parseInt(item)
+    })
+    sum = sum / n
+
+    return sum
+}
+
+let checkArr = (arr) => {
+    if (arr === undefined) return 0
+    switch (arr.length) {
+        case 0:
+            return 0
+        case 1:
+            return arr[0]
+        default:
+            return false
+    }
 }
